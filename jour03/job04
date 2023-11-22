01ba48903de7a60236ffdf730a1ba78aa6b195a0
SOPHIA FRAOUET <147490310+sofia-fraouet@users.noreply.github.com>
2023-11-22 15:39:07 +0100

    fini

---
+++ jour03/job04
@@ -0,0 +1,6 @@
+def GetHello():
+    return "Hello la Plateforme"
+
+
+resultat = GetHello()
+print(resultat)
---
+++ jour03/job07.py
@@ -0,0 +1,18 @@
+def determiner_specialisation(langage):
+    if langage == "JavaScript":
+        print("Tu es un développeur web")
+    elif langage == "python":
+        print("Tu es un développeur IA")
+    elif langage == "java":
+        print("Tu es un développeur logiciel")
+    elif langage == "reactjs":
+        print("Tu es un développeur mobile")
+    else:
+        print("Un jour, je serai le meilleur développeur...")
+
+
+determiner_specialisation("JavaScript")  
+determiner_specialisation("python")  
+determiner_specialisation("java")  
+determiner_specialisation("reactjs")  
+determiner_specialisation("C#")  
